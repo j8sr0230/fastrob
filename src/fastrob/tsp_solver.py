1b35e15728fcfac89f8989ea82ef14edf22426df
j8sr0230 <ronny.scharf08@gmail.com>
2024-03-01 10:47:07 +0100

    Improves tsp_solver.py

--- src/fastrob/tsp_solver.py
+++ src/fastrob/tsp_solver.py
@@ -1,4 +1,9 @@
 from typing import cast
+from itertools import permutations
+
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
 import FreeCADGui as Gui
 import FreeCAD as App
@@ -12,8 +17,19 @@ if __name__ == "__main__":
 
             if hasattr(selection, "Points"):
                 selection: Part.Feature = cast(Part.Feature, selection)
-                points: list[App.Vector] = selection.Points.Points
-                print(points)
+                point_vectors: list[App.Vector] = selection.Points.Points
+                points_array: np.ndarray = np.array(point_vectors)
+                print(points_array)
+
+                G: nx.Graph = nx.Graph()
+                G.add_node(points_array)
+
+                fig, ax = plt.subplots(figsize=(10, 7))
+                nx.draw_networkx(G, position=points_array)
+                plt.show()
+
+                # start, *rest, end = points_array
+                # paths: list[tuple] = [(start, *path, end) for path in permutations(rest)]
 
             else:
                 print("Selection has no points.")
