0afff867fdcacd80bcd222804869cf4e88cb9446
j8sr0230 <ronny.scharf08@gmail.com>
2024-03-01 14:45:58 +0100

    Improves tsp_solver.py

--- src/fastrob/tsp_solver.py
+++ src/fastrob/tsp_solver.py
@@ -19,21 +19,31 @@ if __name__ == "__main__":
                 point_vectors: list[App.Vector] = selection.Points.Points
 
                 point_map: dict[int, App.Vector] = {idx: pos[:-1] for idx, pos in enumerate(point_vectors)}
+                point_list: list[tuple[int, dict]] = [
+                    (idx, {"pos": tuple(pos[:-1])}) for idx, pos in enumerate(point_vectors)
+                ]
 
-                G: nx.Graph = nx.complete_graph(point_map.keys())
-                for node in G:
-                    edge_dict: dict[int, dict] = G[node]
+                # G: nx.Graph = nx.cycle_graph(point_map.keys())
+                G: nx.Graph = nx.Graph()
+                G.add_nodes_from(point_list)
+
+                # print(nx.geometric_edges(G, radius=3))
+                G.add_edges_from(nx.geometric_edges(G, radius=4))
+                # nx.draw(G, pos=point_map, node_size=10, with_labels=False)
+                # plt.show()
+
+                connected_graphs: list[nx.Graph] = [G.subgraph(c).copy() for c in nx.connected_components(G)]
+                # nx.draw(connected_graphs[0], pos=point_map, node_size=10, with_labels=False)
+                # plt.show()
+
+                for node in connected_graphs[0]:
+                    edge_dict: dict[int, dict] = connected_graphs[0][node]
                     for neighbour in edge_dict.keys():
                         dist: float = point_vectors[node].distanceToPoint(point_vectors[neighbour])
-                        G[node][neighbour]["weight"] = dist
-
-                # print(G[0])
-
-                nx.draw(G, pos=point_map, node_size=10, with_labels=False)
+                        connected_graphs[0][node][neighbour]["weight"] = dist
 
                 tsp: Callable = nx.approximation.traveling_salesman_problem
-                solution: list[int] = tsp(G, nodes=point_map.keys())
-
+                solution: list[int] = tsp(connected_graphs[0], nodes=connected_graphs[0].nodes)
                 H: nx.Graph = nx.Graph()
                 nx.add_path(H, solution)
                 nx.draw(H, pos=point_map, node_size=10, with_labels=False)
