814b7f7ea48837a2414fbd13e936e46c5501c4c6
j8sr0230 <ronny.scharf08@gmail.com>
2024-03-01 12:36:45 +0100

    Improves tsp_solver.py

--- src/fastrob/tsp_solver.py
+++ src/fastrob/tsp_solver.py
@@ -1,6 +1,6 @@
-from typing import cast
+from typing import cast, Callable
 
-import numpy as np
+# import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -20,12 +20,24 @@ if __name__ == "__main__":
 
                 point_map: dict[int, App.Vector] = {idx: pos[:-1] for idx, pos in enumerate(point_vectors)}
 
-                G: nx.Graph = nx.Graph()
-                G.add_nodes_from(point_map.keys())
+                G: nx.Graph = nx.complete_graph(point_map.keys())
+                for node in G:
+                    edge_dict: dict[int, dict] = G[node]
+                    for neighbour in edge_dict.keys():
+                        dist: float = point_vectors[node].distanceToPoint(point_vectors[neighbour])
+                        G[node][neighbour]["weight"] = dist
 
-                nx.draw(G, pos=point_map, node_size=10, with_labels=False)
+                # print(G[0])
+
+                # nx.draw(G, pos=point_map, node_size=10, with_labels=False)
+
+                tsp: Callable = nx.approximation.traveling_salesman_problem
+                solution: list[int] = tsp(G, nodes=point_map.keys())
+
+                H: nx.Graph = nx.Graph()
+                nx.add_path(H, solution)
+                nx.draw(H, pos=point_map, node_size=10, with_labels=False)
                 plt.show()
-
             else:
                 print("Selection has no points.")
         else:
