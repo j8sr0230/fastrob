27fb2bda7e0ecb87e7ac6a009cd547dfc58cae6c
j8sr0230 <ronny.scharf08@gmail.com>
2024-03-03 16:26:01 +0100

    Improves tsp_solver.py

--- src/fastrob/face_slicer.py
+++ src/fastrob/face_slicer.py
@@ -20,88 +20,68 @@ if __name__ == "__main__":
             if hasattr(selection, "Shape"):
                 selection: Part.Feature = cast(Part.Feature, selection)
                 faces: list[Part.Face] = selection.Shape.Faces
-                wires: list[Part.Wire] = selection.Shape.Wires
 
-                if 0 < len(faces) <= len(wires):
+                if len(faces) > 0:
                     face: Part.Face = faces[0]
                     rot_face: Part.Face = face.copy()
                     rot_face.rotate(face.CenterOfGravity, App.Vector(0, 0, 1), -BB_ANGLE_DEG)
                     bb: App.BoundBox = rot_face.optimalBoundingBox()
-                    bb: np.ndarray = np.array([[bb.XMin, bb.YMin, bb.ZMin], [bb.XLength, bb.YLength, bb.ZLength]])
-                    bb_offset: np.ndarray = np.array([
-                        bb[0] - np.array([BB_OFFSET, BB_OFFSET, 0]),
-                        bb[1] + 2 * np.array([BB_OFFSET, BB_OFFSET, 0])
-                    ])
+                    bb.enlarge(BB_OFFSET)
+                    bb.move(App.Vector(0, 0, BB_OFFSET))
 
-                    bb_v0: App.Vector = App.Vector(bb_offset[0])
-                    bb_v1: App.Vector = App.Vector(bb_offset[0] + np.array([bb_offset[1][0], 0, 0]))
-                    bb_v2: App.Vector = App.Vector(bb_offset[0] + np.array([bb_offset[1][0], bb_offset[1][1], 0]))
-                    bb_v3: App.Vector = App.Vector(bb_offset[0] + np.array([0, bb_offset[1][1], 0]))
-
-                    bb_x_bottom: Part.Edge = Part.Edge(Part.LineSegment(bb_v0, bb_v1))
-                    bb_x_top: Part.Edge = Part.Edge(Part.LineSegment(bb_v3, bb_v2))
-
-                    hatch_count: int = int(round(bb_x_bottom.Length / SEAM_WIDTH, 0))
-                    x_bottom_points: list[App.Vector] = bb_x_bottom.discretize(Number=hatch_count)
-                    x_top_points: list[App.Vector] = bb_x_top.discretize(Number=hatch_count)
-
-                    hatch_points: list[tuple[App.Vector, App.Vector]] = list(zip(x_bottom_points, x_top_points))
-                    hatch_lines: list[Part.Edge] = []
-                    for point_set in hatch_points:
-                        hatch_line: Part.Edge = Part.Edge(Part.LineSegment(point_set[0], point_set[1]))
-                        hatch_lines.append(hatch_line)
-                    hatch_lines: Part.Compound = Part.makeCompound(hatch_lines)
-                    hatch_lines.rotate(face.CenterOfGravity, App.Vector(0, 0, 1), BB_ANGLE_DEG)
-                    # Part.show(hatch_lines)
-
-                    hatch_sections: list[list[Part.Edge]] = []
-                    for hatch_line in hatch_lines.Edges:
-                        sections: list[Part.Edge] = Part.Edge(hatch_line).common(face).Edges
-                        hatch_sections.append(sections)
-                    # print(hatch_sections)
-
-                    hatch_sections_compound: Part.Compound = Part.makeCompound(
-                        list(itertools.chain.from_iterable(hatch_sections))
+                    bb_left_bottom: App.Vector = App.Vector(bb.XMin, bb.YMin, bb.ZMin)
+                    bb_bottom: Part.Edge = Part.Edge(
+                        Part.LineSegment(bb_left_bottom, bb_left_bottom + App.Vector(bb.XLength, 0, 0))
                     )
-                    Part.show(hatch_sections_compound)
 
-                    section_groups: list[list[Part.Edge]] = [hatch_sections.pop(0)]
-                    section_count: int = len(section_groups[0])
+                    hatch_count: int = int(round(bb_bottom.Length / SEAM_WIDTH, 0))
+                    hatch_starts: list[App.Vector] = bb_bottom.discretize(Number=hatch_count)
+                    hatch: list[Part.Edge] = []
+                    for start_point in hatch_starts:
+                        hatch.append(
+                            Part.Edge(Part.LineSegment(start_point, start_point + App.Vector(0, bb.YLength)))
+                        )
+                    hatch: Part.Compound = Part.makeCompound(hatch)
+                    hatch.rotate(face.CenterOfGravity, App.Vector(0, 0, 1), BB_ANGLE_DEG)
 
-                    sorted_sections: list[list[list[Part.Edge]]] = []
-                    while hatch_sections:
-                        next_section_grp: list[Part.Edge] = hatch_sections.pop(0)
-                        next_section_count: int = len(next_section_grp)
+                    trimmed_hatch: list[list[Part.Edge]] = []
+                    for hatch_line in hatch.Edges:
+                        common: list[Part.Edge] = hatch_line.common(face).Edges
+                        if len(common) > 0:
+                            trimmed_hatch.append(common)
 
-                        if section_count == next_section_count:
-                            section_groups.append(next_section_grp)
+                    trimmed_hatch_comp: Part.Compound = Part.makeCompound(
+                        list(itertools.chain.from_iterable(trimmed_hatch))
+                    )
+
+                    hatch_groups: list[list[list[Part.Edge]]] = []
+                    hatch_group: list[list[Part.Edge]] = [trimmed_hatch.pop(0)]
+                    hatch_group_length: int = len(hatch_group[0])
+                    while trimmed_hatch:
+                        next_hatch_grp: list[Part.Edge] = trimmed_hatch.pop(0)
+                        next_hatch_grp_length: int = len(next_hatch_grp)
+
+                        if hatch_group_length == next_hatch_grp_length:
+                            hatch_group.append(next_hatch_grp)
                         else:
-                            sorted_sections.append(section_groups)
-                            section_groups: list[list[Part.Edge]] = [next_section_grp]
-                        section_count: int = next_section_count
+                            hatch_groups.append(hatch_group)
+                            hatch_group: list[list[Part.Edge]] = [next_hatch_grp]
+                            hatch_group_length: int = next_hatch_grp_length
+                    else:
+                        hatch_groups.append(hatch_group)
 
-                    print(sorted_sections)
+                    paths: list[list[Part.Edge]] = []
+                    while hatch_groups:
+                        hatch_group: list[list[Part.Edge]] = hatch_groups.pop(0)
+                        if len(hatch_group[0]) == 1:
+                            paths.append(list(itertools.chain.from_iterable(hatch_group)))
+                        else:
+                            zipped_paths: list[tuple[Part.Edge]] = list(zip(*hatch_group))
+                            print(list(zipped_paths))
 
-                    # section_count: int = 0
-                    # path_sections: list[Part.Edge] = []
-                    # for hatch_idx in hatch_sections_map.keys():
-                    #     section_count: int = len(hatch_sections_map[hatch_idx])
-                    #     for next_hatch_idx in
-                    #     if hatch_idx + 1 < len(hatch_sections_map.keys()):
+                            #paths.append(list(zip(*hatch_group)))
 
-                    #     trimmed_hatch: list[Part.Edge] = []
-                    #     section_count: int = len(hatch_sections_map[hatch_idx])
-                    #     while hatch_idx < len(hatch_sections_map.keys()) - 1:
-                    #         pass
-                    #
-                    #
-                    #         next_hatch_count: int =
-                    #         while hatch_count =
-                    #
-                    #         current_segment: Part.Edge = hatch_sections_map[hatch_idx].pop(0)
-                    #
-                    #
-                    #         print(hatch_count)
+                    # print(paths)
 
                 else:
                     print("Selection has no wires.")
