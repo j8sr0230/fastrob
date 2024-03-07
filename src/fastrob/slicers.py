c48c1a196e1a715411c128379e006c6e98fffc4e
j8sr0230 <ronny.scharf08@gmail.com>
2024-03-07 14:50:58 +0100

    Improves slicers.py

--- src/fastrob/slicers.py
+++ src/fastrob/slicers.py
@@ -13,19 +13,11 @@ def layers(solid: Part.Solid, layer_height: float) -> list[Part.Face]:
     bb: App.BoundBox = solid.optimalBoundingBox()
     layer_heights: np.ndarray = np.arange(bb.ZMin, bb.ZMax, layer_height)
 
-    wires: list[list[Part.Wire]] = []
-    for z in layer_heights:
-        wires.append(solid.slice(App.Vector(0, 0, 1), z))
-
-    shapes: list[Part.Shape] = []
-    while wires:
-        wire_set: list[Part.Wire] = wires.pop(0)
-        shapes.append(Part.makeFace(wire_set, "Part::FaceMakerBullseye"))
-
     faces: list[Part.Face] = []
-    while shapes:
-        faces.extend(shapes.pop(0).Faces)
-
+    for z_level in layer_heights:
+        wire_set: list[Part.Wire] = solid.slice(App.Vector(0, 0, 1), z_level)
+        face: Part.Shape = Part.makeFace(wire_set, "Part::FaceMakerBullseye")
+        faces.extend(face.Faces)
     return faces
 
 
@@ -39,33 +31,23 @@ def _offset_faces(face: Part.Face, offset: float) -> list[Part.Face]:
 
     try:
         outer_offset: Part.Face = outer_face.makeOffset2D(-offset, 0, False, False, False)
-        inner_offset: Part.Shape = inner_comp.makeOffset2D(offset, 0, False, False, False)
+        inner_offset: Part.Shape = inner_comp.makeOffset2D(-offset, 0, False, False, False)
         cut: Part.Shape = outer_offset.cut(inner_offset)
         if cut.isValid():
-            result: list[Part.Face] = cut.Faces
+            return cut.Faces
         else:
-            print("Offset creation failed.")
-            result: list[Part.Face] = []
-    except Part.OCCError:
-        print("Offset creation failed.")
-        result: list[Part.Face] = []
-    except App.Base.CADKernelError:  # noqa
-        print("Offset creation failed.")
-        result: list[Part.Face] = []
-
-    return result
+            raise ValueError("Offset creation failed.")
+    except (Part.OCCError, App.Base.CADKernelError):  # noqa
+        raise ValueError("Offset creation failed.")
 
 
 def offset_faces(face: Part.Face, offsets: list[float]) -> list[list[Part.Face]]:
-    accumulated_offsets: list[float] = list(itertools.accumulate(offsets))
-
     result: list[list[Part.Face]] = []
-    for offset in list(accumulated_offsets):
+    for offset in itertools.accumulate(offsets):
         new_offset_faces: list[Part.Face] = _offset_faces(face, offset)
-        if len(new_offset_faces) > 0:
+        if new_offset_faces:
             result.append(new_offset_faces)
-
-    return result if len(result) > 0 else [[face]]
+    return result
 
 
 def zig_zag_wires(face: Part.Face, angle_deg: float, seam_width: float, connected: bool) -> list[Part.Wire]:
@@ -76,87 +58,85 @@ def zig_zag_wires(face: Part.Face, angle_deg: float, seam_width: float, connecte
     bb.move(App.Vector(0, 0, BB_OFFSET))
 
     bb_left_bottom: App.Vector = App.Vector(bb.XMin, bb.YMin, bb.ZMin)
-    bb_left: Part.Edge = Part.Edge(Part.LineSegment(
+    bb_edge: Part.Edge = Part.Edge(Part.LineSegment(
         bb_left_bottom,
         bb_left_bottom + App.Vector(0, bb.YLength, 0)
     ))
 
-    hatch_count: int = int(round(bb_left.Length / seam_width, 0))
-    hatch_starts: list[App.Vector] = bb_left.discretize(Number=hatch_count)
-    hatch: list[Part.Edge] = []
-    for start_point in hatch_starts:
-        hatch.append(Part.Edge(Part.LineSegment(
-            start_point,
-            start_point + App.Vector(bb.XLength, 0)
-        )))
-    hatch: Part.Compound = Part.makeCompound(hatch)
-    hatch.rotate(face.CenterOfGravity, App.Vector(0, 0, 1), angle_deg)
+    hatch_count: int = int(round(bb_edge.Length / seam_width, 0))
+    hatch_starts: list[App.Vector] = bb_edge.discretize(Number=hatch_count)
+    hatch: list[Part.Edge] = [
+        Part.Edge(Part.LineSegment(start, start + App.Vector(bb.XLength, 0))) for start in hatch_starts
+    ]
 
-    trimmed_hatch: list[list[Part.Edge]] = []
-    for hatch_line in hatch.Edges:
-        common: list[Part.Edge] = hatch_line.common(face).Edges
-        if len(common) > 0:
-            trimmed_hatch.append(common)
+    hatch_compound: Part.Compound = Part.makeCompound(hatch)
+    hatch_compound.rotate(face.CenterOfGravity, App.Vector(0, 0, 1), angle_deg)
 
-    section_groups: list[list[list[Part.Edge]]] = []
-    section_grp: list[list[Part.Edge]] = [trimmed_hatch.pop(0)]
-    section_grp_count: int = len(section_grp[0])
-    while trimmed_hatch:
-        next_hatch_grp: list[Part.Edge] = trimmed_hatch.pop(0)
-        next_hatch_grp_length: int = len(next_hatch_grp)
+    trimmed_hatch: list[list[Part.Edge]] = [
+        hatch_line.common(face).Edges for hatch_line in hatch_compound.Edges if len(hatch_line.common(face).Edges) > 0
+    ]
 
-        if section_grp_count == next_hatch_grp_length:
-            section_grp.append(next_hatch_grp)
+    if len(trimmed_hatch) > 0:
+        section_groups: list[list[list[Part.Edge]]] = []
+        section_grp: list[list[Part.Edge]] = [trimmed_hatch.pop(0)]
+        section_grp_count: int = len(section_grp[0])
+        while trimmed_hatch:
+            next_hatch_grp: list[Part.Edge] = trimmed_hatch.pop(0)
+            next_hatch_grp_length: int = len(next_hatch_grp)
+
+            if section_grp_count == next_hatch_grp_length:
+                section_grp.append(next_hatch_grp)
+            else:
+                section_groups.append(section_grp)
+                section_grp: list[list[Part.Edge]] = [next_hatch_grp]
+                section_grp_count: int = next_hatch_grp_length
         else:
             section_groups.append(section_grp)
-            section_grp: list[list[Part.Edge]] = [next_hatch_grp]
-            section_grp_count: int = next_hatch_grp_length
-    else:
-        section_groups.append(section_grp)
 
-    sorted_section_groups: list[list[Part.Edge]] = []
-    while section_groups:
-        section_grp: list[list[Part.Edge]] = section_groups.pop(0)
-        if len(section_grp[0]) == 1:
-            sorted_section_groups.append(list(itertools.chain.from_iterable(section_grp)))
-        else:
-            zipped_sections: list[tuple[Part.Edge]] = list(zip(*section_grp))
-            zipped_sections: list[list[Part.Edge]] = [list(tpl) for tpl in zipped_sections]
-            sorted_section_groups.extend(zipped_sections)
+        sorted_section_groups: list[list[Part.Edge]] = []
+        while section_groups:
+            section_grp: list[list[Part.Edge]] = section_groups.pop(0)
+            if len(section_grp[0]) == 1:
+                sorted_section_groups.append(list(itertools.chain.from_iterable(section_grp)))
+            else:
+                zipped_sections: list[tuple[Part.Edge]] = list(zip(*section_grp))
+                zipped_sections: list[list[Part.Edge]] = [list(tpl) for tpl in zipped_sections]
+                sorted_section_groups.extend(zipped_sections)
 
-    result: list[Part.Wire] = []
-    while sorted_section_groups:
-        sorted_section_grp: list[Part.Edge] = sorted_section_groups.pop(0)
+        result: list[Part.Wire] = []
+        while sorted_section_groups:
+            sorted_section_grp: list[Part.Edge] = sorted_section_groups.pop(0)
 
-        if connected:
-            connectors: list[Part.Edge] = []
-            section_len: int = len(sorted_section_grp)
+            if connected:
+                connectors: list[Part.Edge] = []
 
-            for idx, edge in enumerate(sorted_section_grp):
-                if idx < section_len - 1:
-                    next_edge: Part.Edge = sorted_section_grp[idx + 1]
+                for idx, edge in enumerate(sorted_section_grp):
+                    if idx < len(sorted_section_grp) - 1:
+                        next_edge: Part.Edge = sorted_section_grp[idx + 1]
 
+                        if idx % 2 == 0:
+                            start: App.Vector = App.Vector(edge.Vertexes[1].Point)
+                            end: App.Vector = App.Vector(next_edge.Vertexes[1].Point)
+                        else:
+                            start: App.Vector = App.Vector(edge.Vertexes[0].Point)
+                            end: App.Vector = App.Vector(next_edge.Vertexes[0].Point)
+
+                        connectors.append(Part.Edge(Part.LineSegment(start, end)))
+                result.append(Part.Wire(Part.__sortEdges__(sorted_section_grp + connectors)))
+
+            else:
+                for idx, edge in enumerate(sorted_section_grp):
                     if idx % 2 == 0:
-                        start: App.Vector = App.Vector(edge.Vertexes[1].Point)
-                        end: App.Vector = App.Vector(next_edge.Vertexes[1].Point)
+                        edge_points: list[App.Vector] = [App.Vector(v.Point) for v in edge.Vertexes]
+                        edge_points.reverse()
+                        result.append(Part.Wire(
+                            [Part.Edge(Part.LineSegment(edge_points[0], edge_points[1]))]
+                        ))
                     else:
-                        start: App.Vector = App.Vector(edge.Vertexes[0].Point)
-                        end: App.Vector = App.Vector(next_edge.Vertexes[0].Point)
-
-                    connectors.append(Part.Edge(Part.LineSegment(start, end)))
-            result.append(Part.Wire(Part.__sortEdges__(sorted_section_grp + connectors)))
-
-        else:
-            for idx, edge in enumerate(sorted_section_grp):
-                if idx % 2 == 0:
-                    edge_points: list[App.Vector] = [App.Vector(v.Point) for v in edge.Vertexes]
-                    edge_points.reverse()
-                    result.append(Part.Wire(
-                        [Part.Edge(Part.LineSegment(edge_points[0], edge_points[1]))]
-                    ))
-                else:
-                    result.append(Part.Wire([edge]))
-    return result
+                        result.append(Part.Wire([edge]))
+        return result
+    else:
+        return []
 
 
 def trim_wires(wires: list[Part.Wire], clean_distance: float) -> list[Part.Wire]:
@@ -190,9 +170,7 @@ def trim_wires(wires: list[Part.Wire], clean_distance: float) -> list[Part.Wire]
         try:
             cutter: Part.Shape = cut_wire.makeOffset2D(-clean_distance, 0, True, False, False)
             trimmed_wire: Part.Shape = invalid_wire.cut(cutter)
-        except Part.OCCError:
-            pass
-        except App.Base.CADKernelError:  # noqa
+        except (Part.OCCError, App.Base.CADKernelError):  # noqa
             pass
 
         if hasattr(trimmed_wire, "Wires") and len(trimmed_wire.Wires) > 0:
@@ -224,7 +202,8 @@ if __name__ == "__main__":
                             target_face: Part.Face = target_faces.pop(0)
                             is_even: bool = not is_even
 
-                            offset_face_list: list[list[Part.Face]] = offset_faces(face=target_face, offsets=[2, 1])
+                            offset_face_list: list[list[Part.Face]] = offset_faces(face=target_face,
+                                                                                   offsets=[2, 2, 2])
                             if len(offset_face_list) > 1:
                                 contour_faces: list[Part.Face] = list(
                                     itertools.chain.from_iterable(offset_face_list[:-1])
@@ -234,10 +213,9 @@ if __name__ == "__main__":
 
                             contour_compound: Part.Compound = Part.Compound(contour_faces)
                             contour_wires: list[Part.Wire] = contour_compound.Wires
-                            # trimmed_contour_wires: list[Part.Wire] = trim_wires(contour_wires, 2)
+                            # contour_wires: list[Part.Wire] = trim_wires(contour_wires, 2)
 
-                            filling_angles_deg: np.ndarray = np.array([-45, 45])
-                            filling_angles_deg: np.ndarray = np.tile(filling_angles_deg, 3)
+                            filling_angle_deg: float = -45 if is_even else 45
                             filling_face_list: list[Part.Face] = offset_face_list[-1]
                             filling_wires: list[list[Part.Wire]] = []
                             for target_face in filling_face_list:
@@ -250,9 +228,10 @@ if __name__ == "__main__":
                                 filling_wires: list[Part.Wire] = list(itertools.chain.from_iterable(filling_wires))
                             else:
                                 filling_wires: list[Part.Wire] = filling_wires[0]
-                            # trimmed_filling_wires: list[Part.Wire] = trim_wires(filling_wires, 1)
+                            # filling_wires: list[Part.Wire] = trim_wires(filling_wires, 1)
 
                             sliced_wires.extend(contour_wires + filling_wires)
+                            # sliced_wires: list[Part.Wire] = trim_wires(sliced_wires, 1)
 
                         Part.show(Part.Compound(sliced_wires))
                 else:
