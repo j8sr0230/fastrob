5c713f6e43a84ac70836db34d2b03ed25474ea15
j8sr0230 <ronny.scharf08@gmail.com>
2024-03-08 21:28:33 +0100

    Improves slicer.py

--- src/fastrob/shapely_slicer.py
+++ src/fastrob/shapely_slicer.py
@@ -1,4 +1,4 @@
-from typing import Any, cast
+from typing import Any, Union, cast
 from math import sqrt
 from itertools import accumulate
 
@@ -27,71 +27,66 @@ GREEN = '#339933'
 RED = '#ff3333'
 BLACK = '#000000'
 
-DISCRETIZE_DISTANCE: float = 1
+DISCRETIZE_DISTANCE: float = 2
 
 
-def plot_poly(polygons: list[Polygon], lines: list[LineString]) -> None:
+def draw_polygon(polygons: list[Union[Polygon, MultiPolygon]]) -> None:
     fig: plt.Figure = plt.figure(1, figsize=SIZE, dpi=90)
     ax: plt.Axes = fig.add_subplot(111)
+    ax.set_title("Multipolygon")
     ax.set_xlabel("X in mm")
     ax.set_ylabel("Y in mm")
     ax.set_aspect("equal")
 
     for idx, polygon in enumerate(polygons):
         if idx == 0:
-            plot_polygon(polygon, ax=ax, facecolor=GRAY, edgecolor=BLUE,  alpha=0.5, add_points=False)
+            plot_polygon(polygon, ax=ax, facecolor=GRAY, edgecolor=BLUE, alpha=0.5, add_points=False)
         elif idx == len(polygons) - 1:
-            plot_polygon(polygon, ax=ax, facecolor=BLUE, edgecolor=BLUE,  alpha=0.5, add_points=False)
+            plot_polygon(polygon, ax=ax, facecolor=BLUE, edgecolor=BLUE, alpha=0.5, add_points=False)
         else:
-            plot_polygon(polygon, ax=ax, facecolor="#fff", edgecolor=BLUE,  alpha=0.5, add_points=False)
-
-    for line in lines:
-        plot_line(line, ax=ax, color="#292929", alpha=0.5, add_points=False)
-
+            plot_polygon(polygon, ax=ax, facecolor="#fff", edgecolor=BLUE, alpha=0.5, add_points=False)
     plt.show()
 
 
-def layer_polygons(solid: Part.Solid, layer_height: float) -> list[Polygon]:
-    bb: App.BoundBox = solid.optimalBoundingBox()
-    layer_heights: np.ndarray = np.arange(bb.ZMin, bb.ZMax, layer_height)
+def cut_planar(solid: Part.Solid, layer_height: float) -> list[MultiPolygon]:
+    bb: App.BoundBox = solid.BoundBox
+    layer_heights: np.ndarray = np.arange(bb.ZMin + layer_height, bb.ZMax + layer_height, layer_height)
 
-    result: list[Polygon] = []
+    layers: list[MultiPolygon] = []
     for layer_height in layer_heights:
-        contours: list[Part.Wire] = solid.slice(App.Vector(0, 0, 1), layer_height)
-        faces: Part.Shape = Part.makeFace(contours, "Part::FaceMakerBullseye")
+        cross_section_wires: list[Part.Wire] = solid.slice(App.Vector(0, 0, 1), layer_height)
+        cross_section_shapes: Part.Shape = Part.makeFace(cross_section_wires, "Part::FaceMakerBullseye")
 
-        for face in faces.Faces:
-            exterior: Part.Wire = cast(Part.Wire, face.OuterWire)
-            interior: list[Part.Wire] = [wire for wire in face.Wires if not wire.isEqual(exterior)]
+        faces: list[Polygon] = []
+        for cross_section_face in cross_section_shapes.Faces:
+            if cross_section_face.isValid() and cross_section_face.Area > 0:
+                exterior_wire: Part.Wire = cast(Part.Wire, cross_section_face.OuterWire)
+                interior_wires: list[Part.Wire] = [
+                    wire for wire in cross_section_face.Wires if not wire.isEqual(exterior_wire)
+                ]
 
-            ext_points: list[tuple] = [tuple(v) for v in exterior.discretize(Distance=DISCRETIZE_DISTANCE)]
-            int_points: list[list[tuple]] = [
-                [tuple(v) for v in wire.discretize(Distance=DISCRETIZE_DISTANCE)] for wire in interior
-            ]
-            result.append(Polygon(shell=ext_points, holes=int_points))
+                ext_points: list[tuple] = [tuple(v) for v in exterior_wire.discretize(Distance=DISCRETIZE_DISTANCE)]
+                int_points: list[list[tuple]] = [
+                    [tuple(v) for v in wire.discretize(Distance=DISCRETIZE_DISTANCE)] for wire in interior_wires
+                ]
+                faces.append(Polygon(shell=ext_points, holes=int_points))
+        layers.append(MultiPolygon(faces))
 
-    return result
+    return layers
 
 
-def offset_polygons(polygons: list[Polygon], offsets: tuple[float, ...]) -> list[list[Polygon]]:
-    result: list[list[Polygon]] = []
+def erode_cross_sections(cross_sections: list[MultiPolygon], offsets: tuple[float, ...]) -> list[list[Union[Polygon, MultiPolygon]]]:
+    eroded_sections: list[list[MultiPolygon]] = []
 
-    for polygon in polygons:
-        poly_result: list[Polygon] = []
+    for cross_section in cross_sections:
+        temp_offsets: list[MultiPolygon] = []
         for offset in accumulate(offsets):
-            offset_poly: Polygon = polygon.buffer(-offset)
-            if not offset_poly.is_empty:
-                poly_result.append(polygon.buffer(
-                    distance=-offset,
-                    quad_segs=16,
-                    cap_style=1,
-                    join_style=1,
-                    mitre_limit=5.0,
-                    single_sided=False
-                ))
+            offset_multi_polygon: Union[MultiPolygon, Polygon] = cross_section.buffer(-offset)
+            if not offset_multi_polygon.is_empty:
+                temp_offsets.append(offset_multi_polygon)
 
-        result.append(poly_result)
-    return result
+        eroded_sections.append(temp_offsets)
+    return eroded_sections
 
 
 def zig_zag_lines(polygon: Polygon, angle_deg: float, seam_width: float, connected: bool) -> list[LineString]:
@@ -147,29 +142,34 @@ if __name__ == "__main__":
                 if len(selection.Shape.Solids) > 0:
                     target_solid: Part.Solid = selection.Shape.Solids[0]
 
-                    layers_polys: list[Polygon] = layer_polygons(solid=target_solid, layer_height=5)
+                    vertical_slices: list[MultiPolygon] = cut_planar(solid=target_solid, layer_height=2)
+                    horizontal_offsets: list[list[MultiPolygon]] = erode_cross_sections(cross_sections=vertical_slices,
+                                                                                        offsets=(0., 2., 2., 1.))
+                    draw_polygon(horizontal_offsets[-1])
+                    print(horizontal_offsets)
+                    print(len(horizontal_offsets))
 
-                    contour_polys: list[list[Polygon]] = offset_polygons(
-                        polygons=layers_polys, offsets=(2., 1.,)
-                    )
-
-                    filling_lines: list[list[LineString]] = []
-                    for contour_poly in contour_polys:
-                        remainder_poly: Polygon = contour_poly[-1]
-
-                        level_lines: list[LineString] = []
-                        if type(remainder_poly) is MultiPolygon:
-                            for poly in cast(MultiPolygon, remainder_poly).geoms:
-                                level_lines.extend(
-                                    zig_zag_lines(polygon=poly, angle_deg=-45, seam_width=2, connected=True)
-                                )
-                        else:
-                            level_lines.extend(
-                                zig_zag_lines(polygon=remainder_poly, angle_deg=-45, seam_width=2, connected=True)
-                            )
-                        filling_lines.append(level_lines)
-
-                    plot_poly(contour_polys[2], filling_lines[2])
+                    # contour_polys: list[list[Polygon]] = offset_polygons(
+                    #     polygons=layers_polys, offsets=(2., 1.,)
+                    # )
+                    #
+                    # filling_lines: list[list[LineString]] = []
+                    # for contour_poly in contour_polys:
+                    #     remainder_poly: Polygon = contour_poly[-1]
+                    #
+                    #     level_lines: list[LineString] = []
+                    #     if type(remainder_poly) is MultiPolygon:
+                    #         for poly in cast(MultiPolygon, remainder_poly).geoms:
+                    #             level_lines.extend(
+                    #                 zig_zag_lines(polygon=poly, angle_deg=-45, seam_width=2, connected=True)
+                    #             )
+                    #     else:
+                    #         level_lines.extend(
+                    #             zig_zag_lines(polygon=remainder_poly, angle_deg=-45, seam_width=2, connected=True)
+                    #         )
+                    #     filling_lines.append(level_lines)
+                    #
+                    # plot_poly(contour_polys[2], filling_lines[2])
 
                 else:
                     print("No solid selected.")
