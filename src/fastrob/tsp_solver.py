7be555e4b9b3e542e9ccc29499fd42e5ed10381d
j8sr0230 <ronny.scharf08@gmail.com>
2024-03-13 11:06:27 +0100

    Improves shapely_slicer.py

--- src/fastrob/tsp_solver.py
+++ src/fastrob/tsp_solver.py
@@ -1,4 +1,5 @@
 from typing import cast, Callable
+from itertools import chain
 
 # import numpy as np
 import networkx as nx
@@ -15,38 +16,62 @@ if __name__ == "__main__":
             selection: App.DocumentObject = Gui.Selection.getSelection()[0]
             print("Selected object:", selection.Label)
 
-            if hasattr(selection, "Points"):
+            if hasattr(selection, "Shape"):
                 selection: Part.Feature = cast(Part.Feature, selection)
-                point_vectors: list[App.Vector] = selection.Points.Points
+                if len(selection.Shape.Edges) > 0:
+                    boundary_edges: list[Part.Edge] = selection.Shape.Edges
+                    boundary_vertexes: list[Part.Vertex] = selection.Shape.Vertexes
 
-                point_attribute_list: list[tuple[int, dict[str, np.ndarray]]] = [
-                    (idx, {"pos": pos[:-1]}) for idx, pos in enumerate(point_vectors)
-                ]
+                    G: nx.Graph = nx.Graph()
+                    for edge in boundary_edges:
+                        v_start: Part.Vertex = edge.firstVertex()
+                        v_end: Part.Vertex = edge.lastVertex()
 
-                G: nx.Graph = nx.Graph()
-                G.add_nodes_from(point_attribute_list)
-                G.add_edges_from(nx.geometric_edges(G, radius=2.7))
-                # nx.draw(G, pos=nx.get_node_attributes(G, "pos"), node_size=10, with_labels=False)
-                # plt.show()
+                        v_start_id: int = [idx for idx, v in enumerate(boundary_vertexes) if v.isSame(v_start)][0]
+                        v_end_id: int = [idx for idx, v in enumerate(boundary_vertexes) if v.isSame(v_end)][0]
 
-                connected_graphs: list[nx.Graph] = [G.subgraph(c).copy() for c in nx.connected_components(G)]
-                # nx.draw(connected_graphs[0], pos=point_map, node_size=10, with_labels=False)
-                # plt.show()
+                        G.add_node(v_start_id, pos=(v_start.X, v_start.Y))
+                        G.add_node(v_end_id, pos=(v_end.X, v_end.Y))
+                        G.add_edge(v_start_id, v_end_id, weight=edge.Length)
 
-                # for node in connected_graphs[0]:
-                #     edge_dict: dict[int, dict] = connected_graphs[0][node]
-                #     for neighbour in edge_dict.keys():
-                #         dist: float = point_vectors[node].distanceToPoint(point_vectors[neighbour])
-                #         connected_graphs[0][node][neighbour]["weight"] = dist
+                    nx.draw(G, pos=nx.get_node_attributes(G, "pos"), node_size=10, with_labels=False)
+                    plt.show()
 
-                tsp: Callable = nx.approximation.traveling_salesman_problem
-                solution: list[int] = tsp(connected_graphs[0], nodes=connected_graphs[0].nodes, cycle=False)
-                H: nx.Graph = nx.Graph()
-                nx.add_path(H, solution)
-                nx.draw(H, pos=nx.get_node_attributes(G, "pos"), node_size=10, with_labels=False)
-                plt.show()
+                else:
+                    print("Selection has no edges.")
+
+
+                    # selection: Part.Feature = cast(Part.Feature, selection)
+                    # point_vectors: list[App.Vector] = selection.Points.Points
+                    #
+                    # point_attribute_list: list[tuple[int, dict[str, np.ndarray]]] = [
+                    #     (idx, {"pos": pos[:-1]}) for idx, pos in enumerate(point_vectors)
+                    # ]
+                    #
+                    # G: nx.Graph = nx.Graph()
+                    # G.add_nodes_from(point_attribute_list)
+                    # G.add_edges_from(nx.geometric_edges(G, radius=2.7))
+                    # # nx.draw(G, pos=nx.get_node_attributes(G, "pos"), node_size=10, with_labels=False)
+                    # # plt.show()
+                    #
+                    # connected_graphs: list[nx.Graph] = [G.subgraph(c).copy() for c in nx.connected_components(G)]
+                    # # nx.draw(connected_graphs[0], pos=point_map, node_size=10, with_labels=False)
+                    # # plt.show()
+                    #
+                    # # for node in connected_graphs[0]:
+                    # #     edge_dict: dict[int, dict] = connected_graphs[0][node]
+                    # #     for neighbour in edge_dict.keys():
+                    # #         dist: float = point_vectors[node].distanceToPoint(point_vectors[neighbour])
+                    # #         connected_graphs[0][node][neighbour]["weight"] = dist
+                    #
+                    # tsp: Callable = nx.approximation.traveling_salesman_problem
+                    # solution: list[int] = tsp(connected_graphs[0], nodes=connected_graphs[0].nodes, cycle=False)
+                    # H: nx.Graph = nx.Graph()
+                    # nx.add_path(H, solution)
+                    # nx.draw(H, pos=nx.get_node_attributes(G, "pos"), node_size=10, with_labels=False)
+                    # plt.show()
             else:
-                print("Selection has no points.")
+                print("Selection has no shape.")
         else:
             print("Nothing selected.")
     else:
