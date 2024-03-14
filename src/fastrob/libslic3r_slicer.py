95728a121de2cf3f8c55e2675408e69c052c4677
j8sr0230 <ronny.scharf08@gmail.com>
2024-03-14 09:11:59 +0100

    Improves libslic3r_slicer.py

--- src/fastrob/libslic3r_slicer.py
+++ src/fastrob/libslic3r_slicer.py
@@ -1,16 +1,25 @@
 import subprocess
 
 
-DEBUG: bool = False
+DEBUG: bool = True
 
 
-p: subprocess.Popen = subprocess.Popen(
-    "prusa-slicer-console.exe --export-gcode ./resources/cuboid.stl",
-    shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
-)
+def slice_stl(path: str) -> None:
+    p: subprocess.Popen = subprocess.Popen(
+        ("prusa-slicer-console.exe "
+         "--fill-pattern concentric "
+         "--export-gcode " +
+         path),
+        shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
 
-if DEBUG:
-    for line in p.stdout.readlines():
-        print(str(line, "utf-8"))
+    if DEBUG:
+        for line in p.stdout.readlines():
+            print(str(line, "utf-8"))
 
-ret_val: int = p.wait()
+        ret_val: int = p.wait()
+        print(ret_val)
+
+
+if __name__ == "__main__":
+    slice_stl(path="./resources/cuboid.stl")
