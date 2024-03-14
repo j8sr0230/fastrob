b6f71139a5947db225cdace4414e387b36088e19
j8sr0230 <ronny.scharf08@gmail.com>
2024-03-14 19:52:56 +0100

    Improves libslic3r_slicer.py

--- src/fastrob/libslic3r_slicer.py
+++ src/fastrob/libslic3r_slicer.py
@@ -5,44 +5,45 @@ DEBUG: bool = True
 
 
 def slice_stl(stl_file_path: str = "", layer_height: float = 2, seam_width: float = 6, perimeters_num: int = 1,
-              fill_density: int = 100, infill_angle: float = 45) -> int:
-    p: subprocess.Popen = subprocess.Popen(
-        (
-                "prusa-slicer-console.exe "  
-                
-                # [ ACTIONS ]
-                "--export-gcode " +
+              fill_density: int = 100, infill_angle: float = 45, infill_anchor_max: int = 100
+              ) -> subprocess.CompletedProcess:
 
-                # [ TRANSFORM ]
-                "--dont-arrange " +
+    prusa_slicer_process: subprocess.CompletedProcess = subprocess.run(
 
-                # [ OPTIONS ]
-                "--nozzle-diameter 10 " +
-                "--layer-height " + str(layer_height) + " " +
-                "--extrusion-width " + str(seam_width) + " " +
-                "--solid-layers 0 " +
-                "--perimeters " + str(perimeters_num) + " " +
-                "--fill-pattern rectilinear " +
-                "--fill-density " + str(fill_density) + "% " +
-                "--fill-angle " + str(infill_angle) + " " +
-                "--infill-anchor-max 0 " +
+        "prusa-slicer-console.exe "  
+        
+        # [ ACTIONS ]
+        "--export-gcode " +
 
-                # [ file.stl ... ]
-                stl_file_path
-        ),
-        shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+        # [ TRANSFORM ]
+        "--dont-arrange " +
+
+        # [ OPTIONS ]
+        "--nozzle-diameter 10 " +
+        "--layer-height " + str(layer_height) + " " +
+        "--extrusion-width " + str(seam_width) + " " +
+        "--solid-layers 0 " +
+        "--perimeters " + str(perimeters_num) + " " +
+        "--fill-pattern rectilinear " +
+        "--fill-density " + str(fill_density) + "% " +
+        "--fill-angle " + str(infill_angle) + " " +
+        "--infill-anchor-max " + str(infill_anchor_max) + " " +
+
+        # [ file.stl ... ]
+        stl_file_path,
+
+        shell=True, capture_output=True, text=True
     )
 
-    if DEBUG:
-        for line in p.stdout.readlines():
-            print(str(line, "utf-8"))
-
-    return p.wait()
+    return prusa_slicer_process
 
 
 if __name__ == "__main__":
     target_path: str = "./resources/cuboid.stl"
-    slice_stl(
-        stl_file_path=target_path, layer_height=3, seam_width=5,
-        perimeters_num=1, fill_density=50, infill_angle=45
+
+    slicer_process: subprocess.CompletedProcess = slice_stl(
+        stl_file_path=target_path, layer_height=2.2, seam_width=6,
+        perimeters_num=2, fill_density=100, infill_angle=45
     )
+
+    print(slicer_process.stdout)
