e9921349a700ba39c5615962d6803ddc97350b31
j8sr0230 <ronny.scharf08@gmail.com>
2024-03-14 17:55:59 +0100

    Improves libslic3r_slicer.py

--- src/fastrob/libslic3r_slicer.py
+++ src/fastrob/libslic3r_slicer.py
@@ -4,7 +4,8 @@ import subprocess
 DEBUG: bool = True
 
 
-def slice_stl(stl_file: str, layer_height: float, seam_width: float, perimeters_num: int, fill_density: int, infill_angle: float) -> None:
+def slice_stl(stl_file_path: str = "", layer_height: float = 2, seam_width: float = 6, perimeters_num: int = 1,
+              fill_density: int = 100, infill_angle: float = 45) -> int:
     p: subprocess.Popen = subprocess.Popen(
         (
                 "prusa-slicer-console.exe "  
@@ -27,7 +28,7 @@ def slice_stl(stl_file: str, layer_height: float, seam_width: float, perimeters_
                 "--infill-anchor-max 0 " +
 
                 # [ file.stl ... ]
-                stl_file
+                stl_file_path
         ),
         shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
     )
@@ -36,16 +37,12 @@ def slice_stl(stl_file: str, layer_height: float, seam_width: float, perimeters_
         for line in p.stdout.readlines():
             print(str(line, "utf-8"))
 
-        ret_val: int = p.wait()
-        print(ret_val)
+    return p.wait()
 
 
 if __name__ == "__main__":
+    target_path: str = "./resources/cuboid.stl"
     slice_stl(
-        stl_file="./resources/cuboid.stl",
-        layer_height=3,
-        seam_width=5,
-        perimeters_num=1,
-        fill_density=50,
-        infill_angle=45
+        stl_file_path=target_path, layer_height=3, seam_width=5,
+        perimeters_num=1, fill_density=50, infill_angle=45
     )
