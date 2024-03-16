3d7dcf928e1bad03b98eb2017c680f5f8cdb6447
j8sr0230 <ronny.scharf08@gmail.com>
2024-03-16 13:18:14 +0100

    Improves libslic3r_slicer.py

--- src/fastrob/libslic3r_slicer.py
+++ src/fastrob/libslic3r_slicer.py
@@ -26,11 +26,11 @@ SUPPORT_CUBICS: str = "supportcubic"
 LIGHT: str = "lightning"
 
 
-def slice_stl(file: str = "", layer_height: float = 2, seam_width: float = 6, overlap: int = 50,
-              perimeters_num: int = 1, fill_pattern: str = RECT, fill_density: int = 100, infill_angle: float = 45,
+def slice_stl(file: str = "", layer_height: float = 2, seam_width: float = 6, overlap: int = 50, perimeters: int = 1,
+              fill_pattern: str = RECT, fill_density: int = 100, infill_angle: float = 45,
               infill_anchor_max: int = 100) -> subprocess.CompletedProcess:
 
-    slice_cmd: str = (
+    cmd: str = (
             "prusa-slicer-console.exe "  # "superslicer_console.exe "  
         
             # [ ACTIONS ]
@@ -43,46 +43,48 @@ def slice_stl(file: str = "", layer_height: float = 2, seam_width: float = 6, ov
             "--nozzle-diameter " + str((overlap / 100) * seam_width) + " " +
             " --first-layer-height " + str(layer_height) + " " +
             "--layer-height " + str(layer_height) + " " +
-            "--first-layer-extrusion-width " + str(((overlap / 100) * seam_width) + (layer_height * (1 - 3.14/4))) + " " +
-            "--extrusion-width " + str(((overlap / 100) * seam_width) + (layer_height * (1 - 3.14/4))) + " " +
+            "--first-layer-extrusion-width " + str((overlap / 100) * seam_width) + " " +
+            "--extrusion-width " + str((overlap / 100) * seam_width) + " " +
             "--solid-layers 0 " +
-            "--perimeters " + str(perimeters_num) + " " +
+            "--perimeters " + str(perimeters) + " " +
             "--fill-pattern " + str(fill_pattern) + " " +
             "--infill-overlap 50% " +
             "--fill-density " + str(fill_density) + "% " +
             "--fill-angle " + str(infill_angle) + " " +
             "--infill-anchor-max " + str(infill_anchor_max) + " " +
             "--skirts 0 " +
+            "--filament-retract-length 0 " +
 
             # [ file.stl ... ]
             file
-
         )
-    print(str(((overlap / 100) * seam_width) + (layer_height * (1 - 3.14/4))))
 
-    prusa_slicer_process: subprocess.CompletedProcess = subprocess.run(
-        args=slice_cmd, shell=True, capture_output=True, text=True
-    )
+    return subprocess.run(args=cmd, shell=True, capture_output=True, text=True)
 
-    return prusa_slicer_process
+
+def slice_offset_zig_zag(file: str = "", layer_height: float = 2, seam_width: float = 6, infill_angle: float = 45
+                         ) -> subprocess.CompletedProcess:
+
+    return slice_stl(file=file, layer_height=layer_height, seam_width=seam_width, overlap=50, perimeters=1,
+                     fill_pattern=RECT, fill_density=100, infill_angle=infill_angle, infill_anchor_max=100)
 
 
 if __name__ == "__main__":
     dir_path: str = os.path.dirname(os.path.realpath(__file__))
     target_stl: str = str(os.path.join(dir_path, "resources", "cuboid"))
 
-    slicer_process: subprocess.CompletedProcess = slice_stl(
-        file=target_stl + ".stl", layer_height=2, seam_width=5, perimeters_num=0, fill_pattern=RECT,
-        fill_density=100, infill_angle=0, infill_anchor_max=0
+    p: subprocess.CompletedProcess = slice_offset_zig_zag(
+        file=target_stl + ".stl", layer_height=2, seam_width=6, infill_angle=45
     )
 
-    print(slicer_process.stdout)
-    print(slicer_process.stderr)
+    print(p.stdout)
+    print(p.stderr)
 
-    if not slicer_process.stderr:
+    if not p.stderr:
         with open(target_stl + ".gcode", "r") as f:
             gcode_str: str = f.read()
 
         gcode: list[GcodeLine] = GcodeParser(gcode=gcode_str, include_comments=False).lines
         for line in gcode:
+            # TODO: Extract paths
             print(line)
