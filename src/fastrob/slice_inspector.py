5cb506bf63eef9ad597a76ce764fb32810f351f2
j8sr0230 <ronny.scharf08@gmail.com>
2024-03-17 20:16:18 +0100

    Adds slice_inspector.py

--- src/fastrob/slice_inspector.py
+++ src/fastrob/slice_inspector.py
@@ -11,15 +11,18 @@ import Part
 
 
 class SliceInspector(QtWidgets.QWidget):
-    def __init__(self, slice_wires: list[Part.Wire], parent: QtWidgets.QWidget = None):
+    def __init__(self, paths: list[Part.Wire], parent: QtWidgets.QWidget = None):
         super().__init__(parent)
 
-        self._wires: list[Part.Wire] = slice_wires
+        self._paths: list[Part.Wire] = paths
 
-        self._wire_heights: list[float] = [wire.Vertexes[0].Z for wire in self._wires]
-        self._wires_by_layers: np.ndarray = np.split(
-            ary=np.array(self._wires), indices_or_sections=np.where(np.diff(self._wire_heights) != 0)[0] + 1
+        self._path_heights: list[float] = [wire.Vertexes[0].Z for wire in self._paths]
+        self._paths_by_layers: np.ndarray = np.split(
+            ary=np.array(self._paths), indices_or_sections=np.where(np.diff(self._path_heights) != 0)[0] + 1
         )
+
+        self._current_layer: Part.Wire = Part.Wire()
+        self._remaining_layers: np.ndarray = np.array([])
 
         self.setWindowTitle("Slice Inspector")
         self.setMinimumWidth(320)
@@ -32,7 +35,7 @@ class SliceInspector(QtWidgets.QWidget):
         self._layer_label: QtWidgets.QLabel = QtWidgets.QLabel("Layer   ")
         self._layer_slider: QtWidgets.QSlider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
         self._layer_slider.setMinimum(1)
-        self._layer_slider.setMaximum(len(self._wires_by_layers))
+        self._layer_slider.setMaximum(len(self._paths_by_layers))
         self._layer_layout.addWidget(self._layer_label)
         self._layer_layout.addWidget(self._layer_slider)
         self._layout.addLayout(self._layer_layout)
@@ -41,35 +44,45 @@ class SliceInspector(QtWidgets.QWidget):
         self._pos_label: QtWidgets.QLabel = QtWidgets.QLabel("Position")
         self._pos_slider: QtWidgets.QSlider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
         self._pos_slider.setMinimum(0)
-        self._pos_slider.setMaximum(len(slice_wires[0].Vertexes))
+        self._pos_slider.setMaximum(len(paths[0].Vertexes))
         self._pos_layout.addWidget(self._pos_label)
         self._pos_layout.addWidget(self._pos_slider)
         self._layout.addLayout(self._pos_layout)
 
         self._info_layout: QtWidgets.QHBoxLayout = QtWidgets.QHBoxLayout()
-        self._info_label: QtWidgets.QLabel = QtWidgets.QLabel("Layer: 30, Pos: (100, 200, 3)")
+        self._info_label: QtWidgets.QLabel = QtWidgets.QLabel("Layer: , Pos: ")
         self._info_label.setAlignment(QtCore.Qt.AlignRight)
         self._info_layout.addWidget(self._info_label)
         self._layout.addLayout(self._info_layout)
 
-        self._current_layer: App.DocumentObject = App.ActiveDocument.addObject("Part::Feature", "current_Layer")
-        self._current_layer.Label = "Current Layer"
-        # self._current_layer.Shape = Part.makeBox(100, 100, 100)
-        self._remaining_layer: App.DocumentObject = App.ActiveDocument.addObject("Part::Feature", "remaining_layers")
-        self._remaining_layer.Label = "Remaining Layers"
-        # self._remaining_layer.Shape = Part.makeBox(100, 100, 100)
+        self._current_layer_obj: App.DocumentObject = App.ActiveDocument.addObject("Part::Feature", "current_Layer")
+        self._current_layer_obj.Label = "Current Layer"
+        self._current_layer_obj.ViewObject.LineColor = (30, 30, 30)
 
-        cast(QtCore.SignalInstance, self._layer_slider.valueChanged).connect(self.on_layer_value_change)
-        cast(QtCore.SignalInstance, self._pos_slider.valueChanged).connect(self.on_pos_value_change)
+        self._remaining_layers_obj: App.DocumentObject = App.ActiveDocument.addObject("Part::Feature",
+                                                                                      "remaining_layers")
+        self._remaining_layers_obj.Label = "Remaining Layers"
+        self._remaining_layers_obj.ViewObject.LineColor = (150, 150, 150)
 
-    def on_layer_value_change(self) -> None:
+        cast(QtCore.SignalInstance, self._layer_slider.valueChanged).connect(self.on_layer_change)
+        cast(QtCore.SignalInstance, self._pos_slider.valueChanged).connect(self.on_pos_change)
+
+    def on_layer_change(self) -> None:
         index: int = self._layer_slider.value()
 
-        remaining_wires: np.ndarray = np.array(self._wires_by_layers[:index], dtype=object)
-        self._remaining_layer.Shape = Part.makeCompound(remaining_wires.flatten())
-        self._info_label.setText("Layer: " + str(index) + ", Pos: (X, Y, Z)")
+        self._current_layer: np.ndarray = np.array(self._paths_by_layers[index - 1], dtype=object)
+        self._current_layer_obj.Shape = Part.makeCompound(self._current_layer.flatten())
 
-    def on_pos_value_change(self) -> None:
+        if index > 1:
+            self._remaining_layers: np.ndarray = np.array(self._paths_by_layers[:index - 1], dtype=object)
+            self._remaining_layers_obj.Shape = Part.makeCompound(self._remaining_layers.flatten())
+        else:
+            self._remaining_layers: np.ndarray = np.array([])
+            self._remaining_layers_obj.Shape = Part.Shape()
+
+        self._info_label.setText("Layer: " + str(index) + ", Pos: ")
+
+    def on_pos_change(self) -> None:
         index: int = self._pos_slider.value()
         print("Position:", index)
 
@@ -83,7 +96,7 @@ if __name__ == "__main__":
             if hasattr(selection, "Shape"):
                 selection: Part.Feature = cast(Part.Feature, selection)
                 if len(selection.Shape.Wires) > 0 and len(selection.Shape.Vertexes) > 0:
-                    slice_inspector: QtWidgets.QWidget = SliceInspector(slice_wires=selection.Shape.Wires)
+                    slice_inspector: QtWidgets.QWidget = SliceInspector(paths=selection.Shape.Wires)
                     slice_inspector.show()
                 else:
                     print("Shape contains no wires.")
