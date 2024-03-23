33bde0dbbb0a02be4269bf826e686ac56547db11
j8sr0230 <ronny.scharf08@gmail.com>
2024-03-23 08:56:43 +0100

    Improves molecule.py

--- src/fastrob/molecule.py
+++ src/fastrob/molecule.py
@@ -1,6 +1,5 @@
-from typing import cast
+from typing import Any, cast
 
-import FreeCADGui as Gui
 import FreeCAD as App
 import Part
 
@@ -8,75 +7,69 @@ from pivy import coin
 
 
 class Molecule:
-    def __init__(self, obj: Part.Feature) -> None:
-        obj.addProperty("App::PropertyVector", "P1", "Line", "Start point").P1 = App.Vector(1, 1, 0)
-        obj.addProperty("App::PropertyVector", "P2", "Line", "End point").P2 = App.Vector(5, 5, 0)
-        obj.Proxy = self
+    def __init__(self, feature_obj: Part.Feature) -> None:
+        feature_obj.addProperty("App::PropertyVector", "P1", "Line", "Start point").P1 = App.Vector(1, 1, 0)
+        feature_obj.addProperty("App::PropertyVector", "P2", "Line", "End point").P2 = App.Vector(5, 5, 0)
+        feature_obj.Proxy = self
 
     # noinspection PyPep8Naming, PyMethodMayBeStatic
-    def onChanged(self, fp: Part.Feature, prop: str) -> None:
+    def onChanged(self, feature_obj: Part.Feature, prop: str) -> None:
         if prop == "P1" or prop == "P2":
-            fp.Shape = Part.makeLine(fp.getPropertyByName("P1"), fp.getPropertyByName("P2"))
+            feature_obj.Shape = Part.makeLine(feature_obj.getPropertyByName("P1"), feature_obj.getPropertyByName("P2"))
 
     # noinspection PyPep8Naming, PyMethodMayBeStatic
-    def execute(self, fp: Part.Feature) -> None:
-        fp.Shape = Part.makeLine(fp.getPropertyByName("P1"), fp.getPropertyByName("P2"))
+    def execute(self, feature_obj: Part.Feature) -> None:
+        feature_obj.Shape = Part.makeLine(feature_obj.getPropertyByName("P1"), feature_obj.getPropertyByName("P2"))
 
 
 class ViewProviderMolecule:
-    def __init__(self, obj: Gui.ViewProviderDocumentObject) -> None:
-        obj.Proxy = self
-        self.ViewObject = obj
+    def __init__(self, view_obj: Any) -> None:
+        view_obj.Proxy = self
+        self.view_obj = view_obj
 
-        sep_1: coin.SoSeparator = coin.SoSeparator()
-        sel_1: coin.SoSelection = coin.SoType.fromName("SoFCSelection").createInstance()
-        # sel1.policy.setValue(coin.SoSelection.SHIFT)
-        sel_1.ref()
-        sep_1.addChild(sel_1)
+        self._sep_1: coin.SoSeparator = coin.SoSeparator()
+        self._sep_1.ref()
+        self._sel_1: coin.SoSelection = coin.SoType.fromName("SoFCSelection").createInstance()
         self._trl_1: coin.SoTranslation = coin.SoTranslation()
-        sel_1.addChild(self._trl_1)
-        sel_1.addChild(coin.SoSphere())
+        self._sel_1.addChild(self._trl_1)
+        self._sel_1.addChild(coin.SoSphere())
+        self._sep_1.addChild(self._sel_1)
+        view_obj.RootNode.addChild(self._sep_1)
 
-        sep_2: coin.SoSeparator = coin.SoSeparator()
-        sel_2: coin.SoSelection = coin.SoType.fromName("SoFCSelection").createInstance()
-        sel_2.ref()
-        sep_2.addChild(sel_2)
+        self._sep_2: coin.SoSeparator = coin.SoSeparator()
+        self._sep_2.ref()
+        self._sel_2: coin.SoSelection = coin.SoType.fromName("SoFCSelection").createInstance()
         self._trl_2: coin.SoTranslation = coin.SoTranslation()
-        sel_2.addChild(self._trl_2)
-        sel_2.addChild(coin.SoSphere())
+        self._sel_2.addChild(self._trl_2)
+        self._sel_2.addChild(coin.SoSphere())
+        self._sep_2.addChild(self._sel_2)
+        view_obj.RootNode.addChild(self._sep_2)
 
-        obj.RootNode.addChild(sep_1)
-        obj.RootNode.addChild(sep_2)
-
-        self._sel_1 = sel_1
-        self._sel_2 = sel_2
-
-        self.updateData(cast(Part.Feature, obj.Object), "P1")
-        self.updateData(cast(Part.Feature, obj.Object), "P2")
+        self.updateData(cast(Part.Feature, view_obj.Object), "P1")
+        self.updateData(cast(Part.Feature, view_obj.Object), "P2")
 
     # noinspection PyPep8Naming
     def getDetailPath(self, sub_name: str, path: coin.SoFullPath, append: bool) -> bool:
-        vobj: Gui.ViewProviderDocumentObject = self.ViewObject
         if append:
-            path.append(vobj.RootNode)
-            path.append(vobj.SwitchNode)
+            path.append(self.view_obj.RootNode)
+            path.append(self.view_obj.SwitchNode)
 
-            mode: int = vobj.SwitchNode.whichChild.getValue()
-            if mode >= 0:
-                mode: coin.SoSeparator = vobj.SwitchNode.getChild(mode)
-                path.append(mode)
+            child_id: int = self.view_obj.SwitchNode.whichChild.getValue()
+            if child_id >= 0:
+                node: coin.SoSeparator = self.view_obj.SwitchNode.getChild(child_id)
+                path.append(node)
                 sub: str = Part.splitSubname(sub_name)[-1]
                 if sub == "Atom1":
                     path.append(self._sel_1)
                 elif sub == "Atom2":
                     path.append(self._sel_2)
                 else:
-                    path.append(mode.getChild(0))
+                    path.append(node.getChild(0))
         return True
 
     # noinspection PyPep8Naming
-    def getElementPicked(self, pp: coin.SoPickedPoint) -> str:
-        path: coin.SoPath = pp.getPath()
+    def getElementPicked(self, picked_point: coin.SoPickedPoint) -> str:
+        path: coin.SoPath = picked_point.getPath()
         if path.findNode(self._sel_1) >= 0:
             return "Atom1"
         if path.findNode(self._sel_2) >= 0:
@@ -84,14 +77,14 @@ class ViewProviderMolecule:
         raise NotImplementedError
 
     # noinspection PyPep8Naming
-    def updateData(self, fp: Part.Feature, prop: str):
+    def updateData(self, feature_obj: Part.Feature, prop: str) -> None:
         if prop == "P1":
             if hasattr(self, "_sel_1"):
-                p: App.Vector = fp.getPropertyByName("P1")
+                p: App.Vector = feature_obj.getPropertyByName("P1")
                 self._trl_1.translation = (p.x, p.y, p.z)
         elif prop == "P2":
             if hasattr(self, "_sel_2"):
-                p: App.Vector = fp.getPropertyByName("P2")
+                p: App.Vector = feature_obj.getPropertyByName("P2")
                 self._trl_2.translation = (p.x, p.y, p.z)
 
     # noinspection PyPep8Naming, PyMethodMayBeStatic
@@ -105,8 +98,8 @@ class ViewProviderMolecule:
 
 if __name__ == "__main__":
     if App.ActiveDocument:
-        a: Part.Feature = cast(Part.Feature, App.ActiveDocument.addObject("Part::FeaturePython", "Molecule"))
-        Molecule(a)
+        a: App.DocumentObject = App.ActiveDocument.addObject("Part::FeaturePython", "Molecule")
+        Molecule(cast(Part.Feature, a))
         ViewProviderMolecule(a.ViewObject)
         App.ActiveDocument.recompute()
 
