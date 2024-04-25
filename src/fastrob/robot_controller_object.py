7e43a29d39e5351eace88f8d609759dae5e15c6d
j8sr0230 <ronny.scharf08@gmail.com>
2024-04-25 14:09:15 +0200

    Improves robot_controller_object.py

--- src/fastrob/robot_controller_object.py
+++ src/fastrob/robot_controller_object.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
-from typing import cast
+from typing import cast, Optional
 
 import importlib
+from math import radians
 
 import FreeCADGui as Gui
 import FreeCAD as App
@@ -10,7 +11,16 @@ import Part
 
 class RobotControllerObject:
     def __init__(self, feature_obj: Part.Feature, robot_grp: App.DocumentObjectGroup) -> None:
-        feature_obj.addProperty("App::PropertyLink", "aRobot", "Kinematic", "Kinematic of the robot")
+        feature_obj.addProperty("App::PropertyLink", "aRobot", "Kinematic", "Robot kinematic")
+        feature_obj.addProperty("App::PropertyEnumeration", "bMode", "Kinematic", "Mode of the robot controller")
+
+        feature_obj.addProperty("App::PropertyAngle", "aA1", "Forward", "Angle in degree")
+        feature_obj.addProperty("App::PropertyAngle", "bA2", "Forward", "Angle in degree")
+        feature_obj.addProperty("App::PropertyAngle", "cA3", "Forward", "Angle in degree")
+        feature_obj.addProperty("App::PropertyAngle", "dA4", "Forward", "Angle in degree")
+        feature_obj.addProperty("App::PropertyAngle", "eA5", "Forward", "Angle in degree")
+        feature_obj.addProperty("App::PropertyAngle", "fA6", "Forward", "Angle in degree")
+
         # feature_obj.addProperty("App::PropertyLength", "bHeight", "Slicing", "Layer height of the slice")
         # feature_obj.addProperty("App::PropertyLength", "cWidth", "Slicing", "Width of the seams")
         # feature_obj.addProperty("App::PropertyInteger", "dPerimeters", "Slicing", "Number of perimeters")
@@ -29,14 +39,19 @@ class RobotControllerObject:
         # feature_obj.addProperty("App::PropertyVector", "bPoint", "Result", "Point belonging to the point index")
 
         feature_obj.aRobot = robot_grp
+        feature_obj.bMode = ["forward", "inverse"]
+
+        feature_obj.aA1 = 0.
+        feature_obj.bA2 = 0.
+        feature_obj.cA3 = 0.
+        feature_obj.dA4 = 0.
+        feature_obj.eA5 = 0.
+        feature_obj.fA6 = 0.
+
         # feature_obj.bHeight = 2.
         # feature_obj.cWidth = 6.
         # feature_obj.dPerimeters = 1
-        # feature_obj.ePattern = [
-        #     "rectilinear", "alignedrectilinear", "grid", "triangles", "stars", "cubic", "line", "concentric",
-        #     "honeycomb", "3dhoneycomb", "gyroid", "hilbertcurve", "archimedeanchords", "conspiratorial",
-        #     "adaptivecubic", "supportcubic", "lightning"
-        # ]
+
         # feature_obj.fDensity = 100
         # feature_obj.gAngle = 45.
         # feature_obj.hAnchor = 10.
@@ -49,19 +64,55 @@ class RobotControllerObject:
         feature_obj.Proxy = self
         self._feature_obj: Part.Feature = feature_obj
 
+    @staticmethod
+    def _travel_grp(group: App.DocumentObjectGroup, first_items: Optional[list[App.Part]] = None) -> list[App.Part]:
+        if first_items is None:
+            first_items: list[App.Part] = []
+
+        if len(group.Group) > 0 and type(group.Group[0]) is App.Part:
+            item: App.Part = cast(App.Part, group.Group[0])
+            first_items.append(item)
+
     def execute(self, feature_obj: Part.Feature) -> None:
         pass
 
     # noinspection PyPep8Naming, PyMethodMayBeStatic, PyUnusedLocal
     def onChanged(self, feature_obj: Part.Feature, prop: str) -> None:
+        robot_grp: App.DocumentObjectGroup = cast(App.DocumentObjectGroup, feature_obj.getPropertyByName("aRobot"))
+
         if not hasattr(self, "_feature_obj"):
             self._feature_obj: Part.Feature = feature_obj
 
-    def dumps(self) -> dict:
-        return dict()
+        if (feature_obj.getPropertyByName("bMode") == "forward" and
+                prop in ("aA1", "bA2", "cA3", "dA4", "eA4", "fA5", "gA6")):
+            angle_rad: float = radians(feature_obj.getPropertyByName(prop))
+            cast(App.Part, robot_grp.Group[0]).Placement.Rotation.Angle = angle_rad
 
-    def loads(self, state: dict) -> None:
-        pass
+        if prop == "bA2" and feature_obj.getPropertyByName("bMode") == "forward":
+            a1_rad: float = radians(feature_obj.getPropertyByName("bA2"))
+            cast(App.Part, robot_grp.Group[0]).Placement.Rotation.Angle = a1_rad
+
+        if prop == "aA1" and feature_obj.getPropertyByName("bMode") == "forward":
+            a1_rad: float = radians(feature_obj.getPropertyByName("aA1"))
+            cast(App.Part, robot_grp.Group[0]).Placement.Rotation.Angle = a1_rad
+
+        if prop == "aA1" and feature_obj.getPropertyByName("bMode") == "forward":
+            a1_rad: float = radians(feature_obj.getPropertyByName("aA1"))
+            cast(App.Part, robot_grp.Group[0]).Placement.Rotation.Angle = a1_rad
+
+        if prop == "aA1" and feature_obj.getPropertyByName("bMode") == "forward":
+            a1_rad: float = radians(feature_obj.getPropertyByName("aA1"))
+            cast(App.Part, robot_grp.Group[0]).Placement.Rotation.Angle = a1_rad
+
+        if prop == "aA1" and feature_obj.getPropertyByName("bMode") == "forward":
+            a1_rad: float = radians(feature_obj.getPropertyByName("aA1"))
+            cast(App.Part, robot_grp.Group[0]).Placement.Rotation.Angle = a1_rad
+
+    # def dumps(self) -> dict:
+    #     return dict()
+    #
+    # def loads(self, state: dict) -> None:
+    #     pass
 
 
 if __name__ == "__main__":
@@ -78,7 +129,7 @@ if __name__ == "__main__":
                 selection: App.DocumentObjectGroup = cast(App.DocumentObjectGroup, selection)
 
                 robot_ctrl_doc_obj: Part.Feature = cast(
-                    Part.Feature, App.ActiveDocument.addObject("Part::FeaturePython", "Robot Controller")
+                    Part.Feature, App.ActiveDocument.addObject("Part::FeaturePython", "Controller")
                 )
                 RobotControllerObject(feature_obj=robot_ctrl_doc_obj, robot_grp=selection)
                 robot_ctrl_doc_obj.ViewObject.Proxy = 0
